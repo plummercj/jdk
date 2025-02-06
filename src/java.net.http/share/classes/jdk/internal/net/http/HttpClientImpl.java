2ad3d59e4eb8882ee46a70c70cc75c9c0d008211
Volkan Yazici <vyazici@openjdk.org>
2025-02-06 11:49:15 +0000

    8345625: Better HTTP connections
    Reviewed-by: skoivu, rhalade, ahgross, dfuchs, jpai, aefimov

--- src/java.net.http/share/classes/jdk/internal/net/http/HttpClientImpl.java
+++ src/java.net.http/share/classes/jdk/internal/net/http/HttpClientImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1634,11 +1634,6 @@ final class HttpClientImpl extends HttpClient implements Trackable {
     @Override
     public Optional<ProxySelector> proxy() {
         return Optional.ofNullable(userProxySelector);
-    }
-
-    // Return the effective proxy that this client uses.
-    ProxySelector proxySelector() {
-        return proxySelector;
     }
 
     @Override
